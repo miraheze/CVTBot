048e0a1b5f80af6698db38a9dcc5efd836edf678
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-28 23:35:45 -0600

    Bump to version 5.0.0

--- src/CVNBot/Program.cs
+++ src/CVNBot/Program.cs
@@ -15,7 +15,7 @@ namespace CVNBot
 {
     class Program
     {
-        const string version = "4.0.3";
+        const string version = "5.0.0";
 
         public static IrcClient irc = new IrcClient();
         public static RCReader rcirc = new RCReader();
