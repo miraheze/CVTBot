8667650099c36fbfbdba6f6827dca261860a0f71
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-06 13:31:19 -0700

    Bump version to 2.2.5

--- src/CVTBot/Program.cs
+++ src/CVTBot/Program.cs
@@ -15,7 +15,7 @@ namespace CVTBot
 {
     internal class Program
     {
-        private const string version = "2.2.1";
+        private const string version = "2.2.5";
 
         public static IrcClient irc = new IrcClient();
         public static RCReader rcirc = new RCReader();
